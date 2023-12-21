11f77a35efcd1ee8adf117917757710248949e55
root <peredemacron@gmail.com>
2023-12-21 14:21:29 +0000

    fix task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,13 +5,16 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"missing employee id as argument")
+        print("missing employee id as argument")
         sys.exit(1)
 
     URL = "https://jsonplaceholder.typicode.com"
     EMPLOYEE_ID = sys.argv[1]
 
-    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos", params={"_expand": "user"})
+    EMPLOYEE_TODOS = requests.get(
+        f"{URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
     data = EMPLOYEE_TODOS.json()
 
     EMPLOYEE_NAME = data[0]["user"]["username"]
@@ -20,4 +23,9 @@ if __name__ == "__main__":
     with open(fileName, "w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
         for task in data:
-            writer.writerow([EMPLOYEE_ID, EMPLOYEE_NAME, str(task["completed"]), task["title"]])
+            writer.writerow([
+                EMPLOYEE_ID,
+                EMPLOYEE_NAME,
+                str(task["completed"]),
+                task["title"]
+            ])
--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -5,21 +5,28 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"missing employee id as argument")
+        print("missing employee id as an argument")
         sys.exit(1)
 
     URL = "https://jsonplaceholder.typicode.com"
     EMPLOYEE_ID = sys.argv[1]
 
-    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos", params={"_expand": "user"})
+    EMPLOYEE_TODOS = requests.get(
+        f"{URL}/users/{EMPLOYEE_ID}/todos", params={"_expand": "user"}
+    )
     data = EMPLOYEE_TODOS.json()
 
     username = data[0]["user"]["username"]
     USER_TASK = {EMPLOYEE_ID: []}
+
     for task in data:
-        dic_task = {"task": task["title"], "completed": task["completed"],
-                    "username": username}
+        dic_task = {
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": username
+        }
         USER_TASK[EMPLOYEE_ID].append(dic_task)
+
     fileName = f"{EMPLOYEE_ID}.json"
     with open(fileName, "w") as file:
         json.dump(USER_TASK, file)
